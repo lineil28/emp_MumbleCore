017ed394a2e27d3701233a905d193569746812c0
Abduller Maraco <afaruque@stuy.edu>
2021-12-19 16:06:20 -0500

    lab

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
 # emp_MumbleCore
 #### TEAM MumbleCore - Abdullah Faruque, Neil Lin, Russell Goychayev
+## Tests Performed
+Our plan goes as follows...
+1. grasp currentTimeMillis() and use it to create some sort of testing method
+2. have a Comparable array thats When the size will be determined by the input of the tester.
+3. get a number of "trials" which was the number of times <code>BinSearch</code> or <code>LinSearch</code> will search for the target, which will be a random int using <code>SIZE*Math.random()</code>.
+4. record the time needed to find the target <code>TRIALS</code> amount of times. 
+5. Do these tests for numerous sizes. 
+6. analyze.
+
 
 ## Results
 
+| SIZE | TRIALS | BinSearch Time (milliseconds)  | LinSearch Time (milliseconds) |
+|-----------------|:-------------|:---------------:|---------------:|
+| 60        | 10000         | 7      | 11                 |
+| 600       | 10000         | 1      | 1                  |
+| 6000      | 10000         | 2      | 65                 |
+| 60000     | 10000         | 2      | 337                |
+| 600000    | 10000         | 2      | 1305               |
+| 6000000   | 10000         | 2      | 43273              |
+| 60000000  | 10000         | 3      | 141976             |
+| 210000000 | 10000         | 1      | 2466126            |
+
+![alt text](https://github.com/[username]/[reponame]/blob/[branch]/image.jpg?raw=true)
 ## Conclusions
-when the size is below 60, linearsearch is almost always going to be faster(take less time). As the size increases, the time needed to complete a linSearch increases. 
-## Tests Performed
+* As we can see from the table and bar graph above, BinSearch clearly takes less time than LinSearch. As the size increases by multiples of 10, the time needed to complete 10,000 trials of the search grows exponentially. 
+*An interesting observation is that when the size of the <code>Comparable</code> is less than 60, LinSearch typically takes less time than BinSearch.
+* Another observation is the times recorded for when the size was 600. In this scenario, both took 1 millisecond. The only explanation for this was that the <code>target</code> was located at the beginning of the container, meaning that LinSearch would not have to traverse through the 600 length array 10,000 times. 
 
 
 
--- SearchDriver.java
+++ SearchDriver.java
@@ -1,18 +1,52 @@
 public class SearchDriver {
-    public static void main(String[] args) {
-        final int SIZE = 2000000;// as size increases, the linsearch time increases exponentially.
-        final int TRIALS = 1000;//irrelevant when its this high, but good for science //linsearch takes forever with 1000000 trials(supraasrsing minutesns) but binsearch takes about 12 secs
+
+    public static void testtings(int SIZE, int TRIALS){ // as size increases, the linsearch time increases exponentially.
         long start;
         long end;
+        int target = (int)(SIZE * Math.random());
         Comparable[] test;
-//note to self write controls in notes
+        test = new Comparable[SIZE];
+        for(int i = 0; i < SIZE; i++) {
+            test[i] = i;
+        }
+        System.out.println("size is " + SIZE + ", " + TRIALS + " trial being done.");
+        start = System.currentTimeMillis();
+        for(int i = 0; i < TRIALS; i++) {
+            BinSearch.binSearch(test, target);
+        }
+        end = System.currentTimeMillis();
+        long binSearchTime = end - start;
+        System.out.println("BinSearch took " + (end-start) + " milliseconds to do " + TRIALS + " trials.");
+
+        start = System.currentTimeMillis();
+        for(int i = 0; i < TRIALS; i++) {
+            LinSearch.linSearch(test, target);
+        }
+        end = System.currentTimeMillis();
+        long linSearchTime = end - start;
+        System.out.println("LinSearch took " + (end-start) + " milliseconds to do " + TRIALS + " trials.");
+        if (binSearchTime!=0){
+        System.out.println("LinSearch took " + (linSearchTime / binSearchTime) + " times longer.");}
+    }
+    public static void main(String[] args) {
+        testtings(60,10000);
+        testtings(600,10000);
+        testtings(6000,10000);
+        testtings(60000,10000);
+        testtings(600000,10000);
+        testtings(6000000,10000);
+        testtings(60000000,10000);
+        testtings(210000000,10000);
+
+        /*
         test = new Comparable[SIZE];
         for(int i = 0; i < SIZE; i++) {
             test[i] = i;
         }
 
         int target = (int)(SIZE * Math.random());
-
+        System.out.println("size is 60, 100 trials being done.");
+        SIZE=60;
         start = System.currentTimeMillis();
         for(int i = 0; i < TRIALS; i++) {
             BinSearch.binSearch(test, target);
@@ -31,5 +65,26 @@ public class SearchDriver {
 
         System.out.println("LinSearch took " + (linSearchTime / binSearchTime) + " times longer.");
 
+
+        System.out.println("size is 1000000, 100 trials being done.");
+        SIZE=1000000;
+        start = System.currentTimeMillis();
+        for(int i = 0; i < TRIALS; i++) {
+            BinSearch.binSearch(test, target);
+        }
+        end = System.currentTimeMillis();
+        long binSearchTime = end - start;
+        System.out.println("BinSearch took " + (end-start) + " milliseconds to do " + TRIALS + " trials.");
+
+        start = System.currentTimeMillis();
+        for(int i = 0; i < TRIALS; i++) {
+            LinSearch.linSearch(test, target);
+        }
+        end = System.currentTimeMillis();
+        long linSearchTime = end - start;
+        System.out.println("LinSearch took " + (end-start) + " milliseconds to do " + TRIALS + " trials.");
+
+        System.out.println("LinSearch took " + (linSearchTime / binSearchTime) + " times longer.");
+*/
     }
 }
